31a25442c998e34f2a6f53e5b23f6c24425bf595
jjw <794500617@qq.com>
2019-08-24 16:10:59 +0800

    11. 视图函数 request.GET 属性解析 查询参数 QueryDict 支持一键多值 getlist('a') get('b')

--- brequest/urls.py
+++ brequest/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from brequest import views
 urlpatterns = [
 
-    # 1.拼接路径  2008/beijing/
-    url(r'^login/(?P<year>\d{4})/(?P<city>[a-zA-Z]+)/$', views.login_connect),
+    # 1.拼接参数  2008/beijing/
+    url(r'^brequest_connect/(?P<year>\d{4})/(?P<city>[a-zA-Z]+)/$', views.login_connect),
 
+    # 2.解析查询参数 ？a=10&b=20 解析查询参数
+    url(r'^brequest_query/$', views.login_query)
 ]
--- brequest/views.py
+++ brequest/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http.response import HttpResponse
 
-def login_connect(request,city,year):
-    print('解析拼接路径的参数---年份',year)
-    print('解析拼接路径的参数---城市',city)
-    return HttpResponse("1 、request请求对象的传参！")
+# 1.解析拼接参数！
+def login_connect(request, city, year):
+    print('解析拼接路径的参数---年份', year)
+    print('解析拼接路径的参数---城市', city)
+    return HttpResponse("1.解析拼接参数！")
+
+# 2.解析查询参数！
+def login_query(request):
+    # 解析查询参数 request.GET 熟悉
+    params = request.GET
+    print(params)
+    print(type(params))
+    print(params.getlist('a'))
+    print(params.getlist('a')[0])
+    print(params.get('b'))
+
+    return HttpResponse("2.解析查询参数！")

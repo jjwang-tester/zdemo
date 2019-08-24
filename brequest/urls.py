28dc55ecee18a345d55c4da6622432794be40c51
jjw <794500617@qq.com>
2019-08-24 15:52:48 +0800

    10. 请求对象request 参数 ？P<regName>

--- brequest/urls.py
+++ brequest/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from brequest import views
 urlpatterns = [
-    url(r'^login/$', views.login),
+
+    # 1.拼接路径  2008/beijing/
+    url(r'^login/(?P<year>\d{4})/(?P<city>[a-zA-Z]+)/$', views.login_connect),
+
 ]
--- brequest/views.py
+++ brequest/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http.response import HttpResponse
 
-def login(request):
-
+def login_connect(request,city,year):
+    print('解析拼接路径的参数---年份',year)
+    print('解析拼接路径的参数---城市',city)
     return HttpResponse("1 、request请求对象的传参！")
